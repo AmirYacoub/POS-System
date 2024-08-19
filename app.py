017e4013a1f59b40b4d3331a891d0c784983ed4a
your_github_username <your_github_email>
2024-08-20 01:50:30 +0300

    basic api endpoints working

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 from flask import Flask
 from google.cloud import firestore
+import os
+
+# Path to the service account key
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "church-pos-554e25213dcd.json"
 
 app = Flask(__name__)
 
